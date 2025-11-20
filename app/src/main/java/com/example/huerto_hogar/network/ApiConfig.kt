88df6643f4f8e856ffddb7e72a0d2637a9940df6
Cristian Tapia <cris.tapiad@duocuc.cl>
2025-11-20 10:03:30 -0300

    Actualización URLs microservicios AWS - Nuevas instancias
    - Usuario: http://34.193.190.24:8081/
    - Producto: http://34.202.46.121:8081/
    - MongoDB Atlas: Sin cambios (misma dirección)

--- app/src/main/java/com/example/huerto_hogar/network/ApiConfig.kt
+++ app/src/main/java/com/example/huerto_hogar/network/ApiConfig.kt
@@ -4,15 +4,15 @@ package com.example.huerto_hogar.network
  * Configuración de URLs de los microservicios desplegados en AWS
  * 
  * Microservicios:
- * - Usuarios: http://54.211.27.253:8081/swagger-ui/index.html
- * - Productos: http://54.158.158.91:8081/swagger-ui/index.html
+ * - Usuarios: http://34.193.190.24:8081/swagger-ui/index.html#/
+ * - Productos: http://34.202.46.121:8081/swagger-ui/index.html#/
  * 
  * MongoDB Atlas: mongodb+srv://ctapiad_db_user:***@huerto.bi4rvwk.mongodb.net/
  */
 object ApiConfig {
-    // URLs de microservicios en AWS (IPs públicas)
-    const val USER_SERVICE_BASE_URL = "http://54.211.27.253:8081/"
-    const val PRODUCT_SERVICE_BASE_URL = "http://54.158.158.91:8081/"
+    // URLs de microservicios en AWS (IPs públicas - Nuevas instancias)
+    const val USER_SERVICE_BASE_URL = "http://34.193.190.24:8081/"
+    const val PRODUCT_SERVICE_BASE_URL = "http://34.202.46.121:8081/"
     
     // Para desarrollo local (comentado)
     // const val USER_SERVICE_BASE_URL = "http://10.0.2.2:8081/"  // Emulador
